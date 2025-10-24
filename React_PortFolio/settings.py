856fc65288d7df3e8b58b52bad3d64a79d65b892
impphilwebs <imphilwebs@gmail.com>
2025-10-24 11:39:40 +0100

    Add render domain to allowed host in setting.py file

--- React_PortFolio/settings.py
+++ React_PortFolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m09w1nn#0r2cu80byv-a&b%au^8rv8xj#b_57a5_!#nwgs@_54
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-backend-1-2dt3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
